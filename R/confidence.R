cc23c79217361f1a27c90b88207e04a013a4a418
Brian O'Meara <omeara.brian@gmail.com>
2021-02-25 00:23:44 -0500

    hessians and expand.grid

--- R/confidence.R
+++ R/confidence.R
@@ -6,13 +6,13 @@
 #' @param desired.delta How many log likelihood units to deviate from the optimal likelihood from
 #' @param n.points How many points to use
 #' @param verbose If TRUE, print details of the search to the screen
-#' @param likelihood.precision How precisely to calculate likelihoood (less precise=faster)
+#' @param good.only If TRUE, only return the ones within the desired delta
 #' @param ... Other arguments to pass into the likelihood function.
 #' @export 
 #' @examples 
 #' 
 #' @author Brian C. O'Meara
-ComputeConfidenceIntervals <- function(corhmm.object, desired.delta = 2, n.points=5000, verbose=TRUE, likelihood.precision=0.01, ...) {
+ComputeConfidenceIntervals <- function(corhmm.object, desired.delta = 2, n.points=5000, verbose=TRUE, good.only=TRUE, ...) {
 	best.lnl <- corhmm.object$loglik
 	index.mat <- corhmm.object$index.mat
 	raw.rates <- corhmm.object$solution
@@ -26,42 +26,7 @@ ComputeConfidenceIntervals <- function(corhmm.object, desired.delta = 2, n.point
 
 
 	
-	compute_lnlikelihood <- function(par, corhmm.object) {
 
-		corhmm.object$order.test <- FALSE
-		corhmm.object$phy$node.label <- NULL
-		nObs <- length(corHMM:::corProcessData(corhmm.object$data)$ObservedTraits)
-		model.set.final <- corHMM:::rate.cat.set.corHMM.JDB(phy = corhmm.object$phy, data = corhmm.object$data, rate.cat = corhmm.object$rate.cat, ntraits = nObs, model = "ARD")
-		rate.mat <- MK_3state$index.mat
-		rate.mat[rate.mat == 0] <- NA
-		rate <- rate.mat
-		model.set.final$np <- max(rate, na.rm=TRUE)
-		rate[is.na(rate)]=max(rate, na.rm=TRUE)+1
-		model.set.final$rate <- rate
-		model.set.final$index.matrix <- rate.mat
-		model.set.final$Q <- matrix(0, dim(rate.mat)[1], dim(rate.mat)[2])
-		## for precursor type models ##
-		col.sums <- which(colSums(rate.mat, na.rm=TRUE) == 0)
-		row.sums <- which(rowSums(rate.mat, na.rm=TRUE) == 0)
-		drop.states <- col.sums[which(col.sums == row.sums)]
-		if(length(drop.states > 0)){
-		model.set.final$liks[,drop.states] <- 0
-		}
-		result <- corHMM:::dev.corhmm(
-			p = log(par), 
-			phy = corhmm.object$phy, 
-			liks = model.set.final$liks, 
-			Q = model.set.final$Q, 
-			rate = model.set.final$rate, 
-			root.p = corhmm.object$root.p, 
-			rate.cat = corhmm.object$rate.cat, 
-			order.test = corhmm.object$order.test, 
-			lewis.asc.bias = ifelse(any(grepl("lewis.asc.bias", names(corhmm.object))), corhmm.object$lewis.asc.bias, FALSE)
-		)
-
-		#return(dev.corhmm(log(par), corhmm_object$phy, liks=42, Q=42, rate=42, root.p=corhmm.object$root.p, rate.cat=42, order.test=42, lewis.asc.bias=ifelse(any(grepl("lewis.asc.bias", names(corhmm.object))), corhmm.object$lewis.asc.bias, FALSE)))
-		return(-result)
-	}
 
 	# Univariate
 	results<-data.frame(data.frame(matrix(nrow=1, ncol=1+length(par))))
@@ -69,7 +34,7 @@ ComputeConfidenceIntervals <- function(corhmm.object, desired.delta = 2, n.point
 	for(par_index in seq_along(par)) {
 		par <- par.best
 		current.lnl <- best.lnl
-		while(current.lnl > best.lnl - desired.delta) {
+		while(current.lnl > best.lnl - 5*desired.delta) {
 			par[par_index] <- par[par_index]*.99
 			current.lnl <- compute_lnlikelihood(par, corhmm.object)
 			results[nrow(results)+1,] <- c(current.lnl, par)
@@ -78,7 +43,7 @@ ComputeConfidenceIntervals <- function(corhmm.object, desired.delta = 2, n.point
 			}
 		}
 		current.lnl <- best.lnl
-		while(current.lnl > best.lnl - desired.delta) {
+		while(current.lnl > best.lnl - 5*desired.delta) {
 			par[par_index] <- par[par_index]*1.01
 			current.lnl <- compute_lnlikelihood(par, corhmm.object)
 			results[nrow(results)+1,] <- c(current.lnl, par)
@@ -89,52 +54,82 @@ ComputeConfidenceIntervals <- function(corhmm.object, desired.delta = 2, n.point
 	}
 
 	# Multivariate
-	par <- par.best
 	lower <- apply(results[,-1], 2, min, na.rm=TRUE)
 	upper <- apply(results[,-1], 2, max, na.rm=TRUE)
-	starting.row <- nrow(results)
-	print("rbinding")
-	more_results <- matrix(nrow=n.points, ncol=1+length(par))
+	new.params <- GetGridPoints(lower, upper, n.points)
+	new.lnl <- pbapply(new.params, 1, compute_lnlikelihood, corhmm.object)
+
+	more_results <- cbind(matrix(new.lnl, ncol=1), new.params)
 	colnames(more_results) <- colnames(results)
-	print(colnames(results))
-	print(colnames(more_results))
+
+
 	results <- rbind(results, more_results)
-    min.multipliers <- rep(1, length(par))
-    max.multipliers <- rep(1, length(par))
-    for (i in sequence(n.points)) {
-        sim.points<-NA
-        while(is.na(sim.points[1]) | !is.numeric(sim.points[1])) {
-            sim.points<-GenerateValues(par, lower, upper, examined.max=max.multipliers*apply(results[which(results[,1]-min(results[,1], na.rm=TRUE)<=desired.delta),-1], 2, max, na.rm=TRUE), examined.min=min.multipliers*apply(results[which(results[,1]-min(results[,1], na.rm=TRUE)<=desired.delta),-1], 2, min, na.rm=TRUE))
-        }
-        results[i+starting.row,] <- c(compute_lnlikelihood(sim.points, corhmm.object), sim.points)
-        # if(i>5 & restart.mode) {
-        #     if((best.lnl - min(results[,1], na.rm=TRUE) > likelihood.precision ) & allow.restart) {
-        #         results <- results[sequence(i+1),] #stop here and restart
-        #         return(results)
-        #     }
-        # }
-        if (i%%20==0) {
-            for (j in sequence(length(par))) {
-                returned.range <- range(results[which((results[,1]-min(results[,1], na.rm=TRUE))<desired.delta), j+1], na.rm=TRUE)
-                total.range <- range(results[,j+1], na.rm=TRUE)
-                width.ratio <- diff(returned.range)/diff(total.range)
-                if(is.na(width.ratio)) {
-                    width.ratio=1
-                }
-                if(width.ratio > 0.5) { #we are not sampling widely enough
-                    min.multipliers[j] <- min.multipliers[j] * 0.9
-                    max.multipliers[j] <- max.multipliers[j] * 1.1 #expand the range
-                } else {
-                    min.multipliers[j] <- 1
-                    max.multipliers[j] <- 1
-                }
-            }
-        }
-        if (verbose && i%%100==0) {
-            print(paste(i, "of", n.points, "done"))
-        }
-    }
+	results.good.enough <- subset(results, results[,1]>max(results[,1])-desired.delta)
+	if(verbose) {
+		print(apply(results.good.enough[,-1], 2, range))
+	}
+
+	if(good.only) {
+		results <- results.good.enough
+	}
     return(results)
+}
+
+
+HessianConfidence <- function(corhmm.object) {
+	index.mat <- corhmm.object$index.mat
+	raw.rates <- corhmm.object$solution
+	corhmm.object$node.states <- "none" #we don't want to waste time on this
+
+	par <- rep(NA,max(index.mat, na.rm=TRUE))
+	for (i in seq_along(par)) {
+		par[i] <- raw.rates[which(index.mat==i)]
+	}
+	hess <- numDeriv::hessian(compute_lnlikelihood, par, "Richardson", method.args=list(), corhmm.object)
+	fisher_info<-solve(-hess)
+	prop_sigma<-sqrt(diag(fisher_info))
+	return.matrix <- rbind(par-1.96*prop_sigma, par+1.96*prop_sigma)
+	rownames(return.matrix) <- c("lower", "upper")
+	return(return.matrix)
+}
+
+
+# simple function; returns log likelihood
+compute_lnlikelihood <- function(par, corhmm.object) {
+
+	corhmm.object$order.test <- FALSE
+	corhmm.object$phy$node.label <- NULL
+	nObs <- length(corHMM:::corProcessData(corhmm.object$data)$ObservedTraits)
+	model.set.final <- corHMM:::rate.cat.set.corHMM.JDB(phy = corhmm.object$phy, data = corhmm.object$data, rate.cat = corhmm.object$rate.cat, ntraits = nObs, model = "ARD")
+	rate.mat <- MK_3state$index.mat
+	rate.mat[rate.mat == 0] <- NA
+	rate <- rate.mat
+	model.set.final$np <- max(rate, na.rm=TRUE)
+	rate[is.na(rate)]=max(rate, na.rm=TRUE)+1
+	model.set.final$rate <- rate
+	model.set.final$index.matrix <- rate.mat
+	model.set.final$Q <- matrix(0, dim(rate.mat)[1], dim(rate.mat)[2])
+	## for precursor type models ##
+	col.sums <- which(colSums(rate.mat, na.rm=TRUE) == 0)
+	row.sums <- which(rowSums(rate.mat, na.rm=TRUE) == 0)
+	drop.states <- col.sums[which(col.sums == row.sums)]
+	if(length(drop.states > 0)){
+	model.set.final$liks[,drop.states] <- 0
+	}
+	result <- corHMM:::dev.corhmm(
+		p = log(par), 
+		phy = corhmm.object$phy, 
+		liks = model.set.final$liks, 
+		Q = model.set.final$Q, 
+		rate = model.set.final$rate, 
+		root.p = corhmm.object$root.p, 
+		rate.cat = corhmm.object$rate.cat, 
+		order.test = corhmm.object$order.test, 
+		lewis.asc.bias = ifelse(any(grepl("lewis.asc.bias", names(corhmm.object))), corhmm.object$lewis.asc.bias, FALSE)
+	)
+
+	#return(dev.corhmm(log(par), corhmm_object$phy, liks=42, Q=42, rate=42, root.p=corhmm.object$root.p, rate.cat=42, order.test=42, lewis.asc.bias=ifelse(any(grepl("lewis.asc.bias", names(corhmm.object))), corhmm.object$lewis.asc.bias, FALSE)))
+	return(-result)
 }
 
 #' Gets random numbers to try
@@ -169,4 +164,14 @@ GenerateValues <- function(par, lower, upper, max.tries=100, expand.prob=0, exam
     }
     names(new.vals) <- names(par)
     return(new.vals)
+}
+
+GetGridPoints <- function(lower, upper, n.points) {
+	n.grains <- ceiling(n.points^(1/length(lower)))
+	vector.list <- list()
+	for (i in seq_along(lower)) {
+		vector.list[[i]] <- seq(from=lower[i], to=upper[i], length.out=n.grains)
+	}
+	parameter.matrix <- expand.grid(vector.list)
+	return(parameter.matrix)
 }
